a46f6a5f1b04ccef81f072225829677ba4f1f54f
nareo2 <kreakil@naver.com>
2016-07-08 18:18:59 +0900

    apply unittest module

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,23 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
+	def tearDown(self):
+		self.browser.quit()
+	def test_can_start_a_list_and_retrive_it_later(self):
+		#blah blah blah
+		#my game list : darkest dungeon
+		self.browser.get('http://localhost:8000')
+
+		#civilization V
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
+
+		#dungeon crawl stone soup
+		#and so on..
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
