fe6a03d1a9c7b96a739d203aa530e5b4f23c1fa3
nareo2 <kreakil@naver.com>
2017-01-10 02:39:44 +0900

    refactoring ready!

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -33,23 +33,31 @@ class NewVisitorTest(unittest.TestCase):
 		#When I hit enter, the page updates, and now the page lists
 		#"1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
-
+		
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buypeacock feathers' for row in rows),
-			"New to-do item did not appear in table"
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
+		#There is still a text box inviting me to add another item. I
+		#enter "Use peacock feathers to make a fly"
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+
+		#The page updates again, and now shows both iems on my list
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn(
+			'2: Use peacock feathers to make a fly' ,
+			[row.text for row in rows]
 		)
-#There is still a text box inviting me to add another item. I
-#enter "Use peacock feathers to make a fly"
+
+		#I wonders whether the site will remember my list. Then I sees
+		#that the site has generated a unique URL for me -- there is some
+		#explanatory text to that effect.
+
 		self.fail('Finish the test!')
-
-#The page updates again, and now shows both iems on my list
-
-#I wonders whether the site will remember my list. Then I sees
-#that the site has generated a unique URL for me -- there is some
-#explanatory text to that effect.
-
 #I visit that URL - my to-do lists is still there.
 
 #Satisfied, I goes back to sleep
