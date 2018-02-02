4d8ab5f4cf8be627cadafd4400cb06cf0e4f83a1
nareo2 <kreakil@naver.com>
2018-02-02 09:07:41 +0900

    Change django version to 1.8, chapter 4

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,15 +1,5 @@
-from django.shortcuts import redirect, render
-from lists.models import Item
+from django.shortcuts import render
 
+# Create your views here.
 def home_page(request):
-	if request.method == 'POST':
-		Item.objects.create(text= request.POST['item_text'])
-		return redirect('/lists/the-only-list-in-the-world/')
-	
-	items = Item.objects.all()
-	return render(request, 'home.html', {'items':items})
-
-def view_list(request):
-	items = Item.objects.all()
-	return render(request, 'home.html', {'items':items})
-	
+    return render(request, 'home.html')
