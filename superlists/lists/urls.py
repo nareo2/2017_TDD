98520c26aa39fd903e4ba861bfd50383764cb88b
nareo2 <kreakil@naver.com>
2018-02-12 20:32:50 +0900

    Seperate /lists URL

---
+++ superlists/lists/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url, patterns
+from lists import views
+
+urlpatterns = patterns('',
+    url(r'^(\d+)/$',
+        'lists.views.view_list',
+        name='view_list'
+    ),
+    url(r'^(\d+)/add_item$',
+        'lists.views.add_item',
+        name='add_item'
+    ),
+    url(r'^new$', 'lists.views.new_list', name='new_list'),
+)
