d174b465dee60e47954ec00873069027dbed9f3c
nareo2 <kreakil@naver.com>
2018-02-22 05:17:40 +0900

    Complete change

--- superlists/superlists/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url, patterns, include
-from lists import views
-
-urlpatterns = patterns('',
-    url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/', include('lists.urls')),
-)
