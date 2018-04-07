85a14bfc478d0a5f5b52a92210f94644c1103514
nareo2 <kreakil@naver.com>
2018-04-08 08:48:30 +0900

    Refactor list view to handle new item POSTs

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -46,8 +46,6 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/%d/' % (correct_list.id,))
         self.assertEqual(response.context['list'], correct_list)
 
-class NewListTest(TestCase):
-
     def test_saving_a_POST_request(self):
         self.client.post(
             '/lists/new',
@@ -70,7 +68,7 @@ class NewListTest(TestCase):
         correct_list = List.objects.create()
 
         self.client.post(
-            '/lists/%d/add_item' % (correct_list.id,),
+            '/lists/%d/' % (correct_list.id,),
             data={'item_text': 'A new item for an existing list'}
         )
 
@@ -79,16 +77,15 @@ class NewListTest(TestCase):
         self.assertEqual(new_item.text, 'A new item for an existing list')
         self.assertEqual(new_item.list, correct_list)
 
-    def test_redirects_to_list_view(self):
+    def test_POST_redirects_to_list_view(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
         response = self.client.post(
-            '/lists/%d/add_item' % (correct_list.id,),
+            '/lists/%d/' % (correct_list.id,),
             data={'item_text': 'A new item for an existing list'}
         )
-
-        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,),)
 
     def test_validation_errors_are_sent_back_to_home_page_template(self):
         response = self.client.post('/lists/new', data={'item_text': ''})
